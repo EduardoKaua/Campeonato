37a0924fd52a11a3a70a7de79df974a43ffd91f5
Eduardo Kauã de Souza <edubr038@gmail.com>
2022-06-22 23:21:41 -0300

    att

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css"
+
+export default function App() {
+    return <body>
+    <header>
+        <div className="cabeçalho">
+            <br></br>
+            
+        </div>
+    </header>
+    <div>
+        
+    </div>
+    </body>
+}
