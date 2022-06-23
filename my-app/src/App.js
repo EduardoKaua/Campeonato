ff94343a0608f9185336ae48ed9844be991ac433
Eduardo Kauã de Souza <edubr038@gmail.com>
2022-06-23 16:49:00 -0300

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,32 @@
-import "./App.css"
+import "./App.css";
+import React from "react";
 
 export default function App() {
-    return <body>
-        <div className="cabeçalho">
+  return (
+    <body>
+      <div className="cabeçalho">
         <div className="Logo">
-                <h1>ELITI</h1>
-            </div>
+          <h1>ELITI</h1>
         </div>
-        <a href="#">
- <div className="login"> <button>login</button>
-    </div> 
-    </a>   
+      </div>
+      <a href="#">
+        <div className="login">
+          <button>login</button>
+        </div>
+      </a>
 
-    <div className="csgo"> <button> CSGO </button> </div> 
-   <div className="valorant"> <button>VALORANT </button>
-  <div className="free fire"> <button>FREE FIRE </button>
- <div className="lol"> <button>LOL</button>
-
-</div> 
- </div>
-  </div>   
+      <div className="csgo">
+        <button>CSGO</button>
+      </div>
+      <div className="valorant">
+        <button>VALORANT </button>
+      </div>
+      <div className="freefire">
+        <button>FREE FIRE </button>
+      </div>
+      <div className="lol">
+        <button>LOL</button>
+      </div>
     </body>
+  );
 }
