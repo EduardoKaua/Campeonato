9d4683698c1c7fd37dfca0dd6d732baa00b39d57
Gustavo Machado Mette <gustavo.mette@gmail.com>
2022-06-27 17:59:38 -0300

    update

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,16 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
 import cs from "./cs";
-export * from './paginas/Login';
+export * from './pages/Login';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
 ReactDOM.render(
   <React.StrictMode>
     <cs />
