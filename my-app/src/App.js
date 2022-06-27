778b0c42f6b50eab38ed16aa96b86b3bef4b62ea
Eduardo Kauã de Souza <edubr038@gmail.com>
2022-06-27 00:36:54 -0300

    site

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
 import React from "react";
 import csgo from './imagens/csgo.png'
+import valorant from './imagens/valorant.png'
+import freefire from './imagens/free fire.png'
+import lol from './imagens/lol.png'
 
 export default function App() {
 
@@ -11,23 +14,19 @@ export default function App() {
           <h1>ELITI</h1>
         </div>
       </div>
-        <div className="login">
-          <button>login</button>  
-        </div>
-      <div className="csgo">
-        <button>CSGO</button>
-      </div>
-      <div className="valorant">
-        <button>VALORANT </button>
-      </div>
-      <div className="freefire">
-        <button>FREE FIRE </button>
-      </div>
-      <div className="lol">
-        <button>LOL</button>
-      </div>
-
+        
+<div className="csgo">
 <img src={csgo} alt="" />
+</div>
+<div className="valorant">
+<img src={valorant} alt="" />
+</div>
+<div className="freefire">
+<img src={freefire} alt="" />
+</div>
+<div className="lol" >
+<img src={lol} alt="" />
+</div>
 
     </body>
   );
