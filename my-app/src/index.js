20c988d004d306440aace15da2791f23e79c4dad
Gustavo Machado Mette <gustavo.mette@gmail.com>
2022-06-27 20:38:31 -0300

    .

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Cs from "./cs";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Cs />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
